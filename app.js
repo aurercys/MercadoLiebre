257fb2bcce5e72daab2e2563e3c51aebaabb8147
Aurercys Arias <aurercys74@gmail.com>
2022-02-04 09:14:17 -0300

    cambiosdelaterminal

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require ('express');
 const app = express ();
 const path = require('path');
 
-app.listen (3000, () =>{
-     console.log('Servidor Funcionando');
+// app.listen (3000, () =>{
+//      console.log('Servidor Funcionando');
+// });
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Servidor Funcionando');
+
 });
 
 const publicPath = path.resolve(__dirname, 'public');
