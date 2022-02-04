ab30494d4ecc671b98133f54de207f377a2c4c90
Aurercys Arias <aurercys74@gmail.com>
2022-02-04 08:12:57 -0300

    mercadoliebre

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require ('express');
+const app = express ();
+const path = require('path');
+
+app.listen (3000, () =>{
+     console.log('Servidor Funcionando');
+});
+
+const publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath));
+
+app.get ('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views', 'home.html'));
+});
+
+app.get ('/login.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views', 'login.html'));
+});
+
+app.get ('/register.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views', 'register.html'));
+});
+
